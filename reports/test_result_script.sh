95f20640e08d7165c0083442df390d2706f4c646
s.akshay <akshay@shoppinpal.com>
2024-06-06 18:21:03 +0530

    testsuites

--- reports/test_result_script.sh
+++ reports/test_result_script.sh
@@ -8,9 +8,9 @@
 ###################################################################################################
 
 TEST_RESULTS_LOCATION="${1:-/home/runner/work/playwrightWorking/playwrightWorking/test-results}"
-TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/results.xml" | grep "<testng-results")
-echo "IGNORED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')"
-echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')"
-echo "PASSED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')"
+TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/results.xml" | grep "<testsuites")
+echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')"
 echo "FAILED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')"
 echo "SKIPPED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')"
+echo "ERRORS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $12 }')"
+echo "TIME=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $14 }')"
