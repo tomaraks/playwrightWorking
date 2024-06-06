370c0bc8f63caea5db7ae40e77cb00d2f5042388
s.akshay <akshay@shoppinpal.com>
2024-06-06 15:05:32 +0530

    added metrics

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
+
+test("run an api test", async ({ page, request }) => {
+  let response = await request.get(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+
+  let responseBody = await response.json();
+  expect(responseBody).toHaveProperty("completed");
 });
