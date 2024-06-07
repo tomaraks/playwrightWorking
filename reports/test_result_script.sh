8bbe2dd3494086f7a0855b0b3fc093602fdc2e9f
s.akshay <akshay@shoppinpal.com>
2024-06-07 18:08:00 +0530

    adding more metrics

--- reports/test_result_script.sh
+++ reports/test_result_script.sh
@@ -16,3 +16,9 @@ github_actions_skipped_tests{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_
 github_actions_errors{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $12 }')
 github_actions_time{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $14 }')
 EOF
+
+echo " gha.maventest.total=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.failed=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.skipped=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.errors=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $12 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.time=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $14 }')" >> $BUILDEVENT_FILE
